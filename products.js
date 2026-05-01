c0cb944183b16ef79b1385c543547de8f5f9aa24
abemfortin <vwqqq79vr4@privaterelay.appleid.com>
2026-05-01 14:02:25 -0400

    Update menu via admin panel

--- products.js
+++ products.js
@@ -1,13 +1,13 @@
 // products.js — managed via abesbaking.com?admin
 
-const shopClosed = true;
+const shopClosed = false;
 
 const products = [
   {
-    "name": "test item",
+    "name": "Chocolate Toffee Cookie",
     "description": "",
     "price": "$6.99",
-    "stripeLink": "",
+    "stripeLink": "https://buy.stripe.com/6oU6oHd4A8C41Zjf9d5kk0d",
     "inStock": true,
     "imageUrl": ""
   }
